0c49a9b4413701f4364ee51b7569e8f6e4c9a60f
Jashan S <jashan1998s@gmail.com>
2016-07-02 15:47:09 +0000

    All cucumber tests pass

--- app.rb
+++ app.rb
@@ -37,9 +37,19 @@ class HangpersonApp < Sinatra::Base
   # If a guess is repeated, set flash[:message] to "You have already used that letter."
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
-    letter = params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
-    redirect '/show'
+    letter = params[:guess].to_s[0] || ''
+    if !@game.guess(letter)
+      flash[:message] = "You have already used that letter"
+    end
+    
+    if @game.check_win_or_lose == :win
+      redirect '/win'
+    elsif @game.check_win_or_lose == :lose
+      redirect '/lose'
+    else 
+      redirect '/show'
+    end
+    
   end
   
   # Everytime a guess is made, we should eventually end up at this route.
