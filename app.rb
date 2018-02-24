b68008e5a7302e07bf702911493747018816edd6
lenin <vargaslen@gmail.com>
2018-02-24 09:01:23 -0430

    d

--- app.rb
+++ app.rb
@@ -40,21 +40,7 @@ class HangpersonApp < Sinatra::Base
     letter = params[:guess].to_s[0] || ''
     p "params.to_s: #{params.to_s}"
     # handle argument errors too
-    begin
-      if !@game.guess(letter)
-        flash[:message] = "You have already used that letter"
-      end
-      if @game.check_win_or_lose == :win
-        redirect '/win'
-      elsif @game.check_win_or_lose == :lose
-        redirect '/lose'
-      else
-        redirect '/show'
-      end
-    rescue ArgumentError
-      flash[:message] = "Your input was invalid"
-      redirect '/show'
-    end
+
 
 
 
